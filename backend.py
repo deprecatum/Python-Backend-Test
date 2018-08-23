0593c3a7d876d92ce5dd8f3fef07eeeaa91d7d3e
deprecatum <33914332+deprecatum@users.noreply.github.com>
2018-08-23 14:33:35 +0100

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,1 +1,21 @@
 
+import SocketServer
+
+port=8080
+
+#need to make a custom handler to interact with a requests content
+class custom_handler(SocketServer.StreamRequestHandler):
+#use streamrequest instead of base, so its possible to manipulate data as files
+
+    def handle(self):
+        print 'something'
+        #output = self.rfile.readline()
+        #print output
+
+    
+
+
+handler=custom_handler
+
+server=SocketServer.TCPServer(("localhost",port),handler)
+server.serve_forever()
